4faa4463c480f034c3ef6cd458549aeebdc2dab6
James Mikesell <james.mikesell@gmail.com>
2024-08-29 19:07:28 -0400

    moving local device display stats aggregation to service

--- ui/src/app/component/display-stats-device-sensors/display-stats-device-sensors.component.ts
+++ ui/src/app/component/display-stats-device-sensors/display-stats-device-sensors.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject, timer, takeUntil, filter } from 'rxjs';
-import { ControllerOrientationService } from 'src/app/service/controller-orientation.service';
-import { ControllerPathService } from 'src/app/service/controller-path.service';
-import { ControllerRotationRateService } from 'src/app/service/controller-rotation-rate.service';
-import { DeviceSelectService } from 'src/app/service/device-select.service';
-import { GpsSensor, GpsSensorData } from 'src/app/service/sensor-gps.service';
-import { OrientationSensor } from 'src/app/service/sensor-orientation.service';
-import { UnitConverter } from 'src/app/utils/unit-converter';
+import { Subject, takeUntil } from 'rxjs';
+import { DisplayStatsService } from 'src/app/service/display-stats.service';
 import { DisplayStats } from '../display-stats/display-stats.component';
 
 @Component({
@@ -16,25 +10,15 @@ import { DisplayStats } from '../display-stats/display-stats.component';
 })
 export class DisplayStatsDeviceSensorsComponent implements OnInit, OnDestroy {
 
-  displayStatsConfig: DisplayStats;
+  displayStats: DisplayStats;
 
 
   private destroy = new Subject<void>();
-  private gpsHeading: number;
-  private sensorOrientation: OrientationSensor;
-  private sensorLocation: GpsSensor;
-  private speedKts: number;
 
 
   constructor(
-    private controllerOrientation: ControllerOrientationService,
-    private controllerRotationRate: ControllerRotationRateService,
-    private controllerPath: ControllerPathService,
-    deviceSelectService: DeviceSelectService,
-  ){
-    this.sensorOrientation = deviceSelectService.orientationSensor;
-    this.sensorLocation = deviceSelectService.gpsSensor;
-  }
+    private displayStatService: DisplayStatsService,
+  ) { }
 
 
   ngOnDestroy(): void {
@@ -44,39 +28,8 @@ export class DisplayStatsDeviceSensorsComponent implements OnInit, OnDestroy {
 
 
   ngOnInit(): void {
-    timer(0, 1 * 250)
+    this.displayStatService.displayStats
       .pipe(takeUntil(this.destroy))
-      .subscribe(() => {
-        this.updateDisplayStats();
-      });
-
-    this.sensorLocation.locationData
-      .pipe(filter(locationData => !!locationData))
-      .pipe(takeUntil(this.destroy))
-      .subscribe(locationData => this.locationUpdated(locationData));
-  }
-
-
-  private locationUpdated(locationData: GpsSensorData): void {
-    this.speedKts = UnitConverter.mpsToKts(locationData.speedMps);
-    this.gpsHeading = locationData.heading;
-  }
-
-
-  private updateDisplayStats() {
-    this.displayStatsConfig = {
-      headingCurrentCompass: this.sensorOrientation.heading.value.heading,
-      headingCurrentGps: this.gpsHeading,
-      headingCurrentDrift: this.controllerPath.compassDriftDegrees,
-
-      headingDesiredCompass: this.controllerOrientation.desired,
-      headingDesiredGps: this.controllerPath.desiredHeadingToDestination,
-
-      speedKts: this.speedKts,
-
-      controllerRotationRate: this.controllerRotationRate.enabled,
-      controllerOrientation: this.controllerOrientation.enabled,
-      controllerPath: this.controllerPath.enabled,
-    }
+      .subscribe(stats => this.displayStats = stats)
   }
 }
---
+++ ui/src/app/service/display-stats.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, filter, timer } from 'rxjs';
+import { DisplayStats } from '../component/display-stats/display-stats.component';
+import { UnitConverter } from '../utils/unit-converter';
+import { ControllerOrientationService } from './controller-orientation.service';
+import { ControllerPathService } from './controller-path.service';
+import { ControllerRotationRateService } from './controller-rotation-rate.service';
+import { DeviceSelectService } from './device-select.service';
+import { GpsSensor, GpsSensorData } from './sensor-gps.service';
+import { OrientationSensor } from './sensor-orientation.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DisplayStatsService {
+
+  displayStats = new BehaviorSubject<DisplayStats>({} as DisplayStats);
+
+
+  private gpsHeading: number;
+  private sensorOrientation: OrientationSensor;
+  private sensorLocation: GpsSensor;
+  private speedKts: number;
+
+
+  constructor(
+    private controllerOrientation: ControllerOrientationService,
+    private controllerRotationRate: ControllerRotationRateService,
+    private controllerPath: ControllerPathService,
+    deviceSelectService: DeviceSelectService,
+  ) {
+    this.sensorOrientation = deviceSelectService.orientationSensor;
+    this.sensorLocation = deviceSelectService.gpsSensor;
+
+    timer(0, 1 * 250)
+      .subscribe(() => {
+        this.updateDisplayStats();
+      });
+
+    this.sensorLocation.locationData
+      .pipe(filter(locationData => !!locationData))
+      .subscribe(locationData => this.locationUpdated(locationData));
+  }
+
+
+  private locationUpdated(locationData: GpsSensorData): void {
+    this.speedKts = UnitConverter.mpsToKts(locationData.speedMps);
+    this.gpsHeading = locationData.heading;
+  }
+
+
+  private updateDisplayStats() {
+    this.displayStats.next({
+      headingCurrentCompass: this.sensorOrientation.heading.value.heading,
+      headingCurrentGps: this.gpsHeading,
+      headingCurrentDrift: this.controllerPath.compassDriftDegrees,
+
+      headingDesiredCompass: this.controllerOrientation.desired,
+      headingDesiredGps: this.controllerPath.desiredHeadingToDestination,
+
+      speedKts: this.speedKts,
+
+      controllerRotationRate: this.controllerRotationRate.enabled,
+      controllerOrientation: this.controllerOrientation.enabled,
+      controllerPath: this.controllerPath.enabled,
+    })
+  }
+
+
+}
