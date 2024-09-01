4d74c54bab50a9622988ebb9d55960b21a2c2c59
James Mikesell <james.mikesell@gmail.com>
2024-09-01 16:07:57 -0400

    changing BT sending to await finish of send to BT device, and send on a constant interval, also timing out if necessary

--- ui/src/app/service/controller-bt-motor.service.ts
+++ ui/src/app/service/controller-bt-motor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, timer } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { Controller } from './controller';
 
 @Injectable({
@@ -17,16 +17,38 @@ export class ControllerBtMotorService implements Controller<number>, Connectable
   ) {
     window.addEventListener("beforeunload", () => this.disconnect())
 
-    timer(0, 200)
-      .subscribe(() => {
-        if (!this.connected.value)
-          return;
+    void this.initSendTimer();
+  }
 
-        let direction = this.nextPowerLevel > 0 ? Direction.RIGHT : Direction.LEFT;
-        // TODO: this should probably be awaited....
-        // TODO: if awaited, see if there are any circumstances where the await doesn't finish
-        void this.move(direction, Math.abs(this.nextPowerLevel))
-      });
+
+  private async initSendTimer(): Promise<void> {
+    const sendInterval = 200;
+    const timeoutMs = 1000;
+    while (true) {
+      const start = Date.now();
+
+      if (this.connected.value) {
+        const direction = this.nextPowerLevel > 0 ? Direction.RIGHT : Direction.LEFT;
+        let btSendPromise = this.move(direction, Math.abs(this.nextPowerLevel))
+          .catch(e => console.error("error sending bt message", e))
+          .then(() => false)
+
+        let timeoutPromise = this.sleep(timeoutMs)
+          .then(() => true)
+
+        let timedOut = await Promise.race([btSendPromise, timeoutPromise]);
+        if (timedOut === true)
+          console.error(`BT command send timed out after ${timeoutMs}ms`)
+      }
+
+      const remainingTime = sendInterval - (Date.now() - start);
+      await this.sleep(Math.max(0, remainingTime));
+    }
+  }
+
+
+  private async sleep(timeMs: number): Promise<void> {
+    await new Promise(r => setTimeout(r, timeMs))
   }
 
 
@@ -56,7 +78,7 @@ export class ControllerBtMotorService implements Controller<number>, Connectable
     try {
       let device = await navigator.bluetooth.requestDevice(config);
       this.gatt = device.gatt;
-      device.addEventListener("gattserverdisconnected", _event => this.bluetoothDisconnected());
+      device.addEventListener("gattserverdisconnected", () => this.bluetoothDisconnected());
       console.log('Connecting to GATT Server...');
       let server = await device.gatt.connect();
       console.log('Getting Service...');
@@ -65,7 +87,7 @@ export class ControllerBtMotorService implements Controller<number>, Connectable
       this.characteristic = await service.getCharacteristic(CHARACTERISTIC_UUID);
       this.connected.next(true);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
 
